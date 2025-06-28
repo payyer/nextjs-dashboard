681673636602a56105577d23f84899fb5cbcae5a
payyer <quocanhle112@gmail.com>
2025-06-28 12:06:16 +0700

    update and delete invoice

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -6,8 +6,6 @@ import { redirect } from 'next/navigation'
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' })
 
-
-
 const FormSchema = z.object({
   id: z.string(),
   customerId: z.string(),
@@ -34,4 +32,27 @@ export async function createInvoice(formData: FormData) {
 
   revalidatePath('/invoices')
   redirect('/dashboard/invoices')
+}
+
+const UpdateInvoice = FormSchema.omit({ id: true, date: true })
+
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  })
+  const amountInCents = amount * 100;
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `
+  revalidatePath('/invoices')
+  redirect('/dashboard/invoices')
+}
+
+export async function deleteInvoice(id: string) {
+  await sql`DELETE FROM invoices WHERE id = ${id}`
+  revalidatePath('/dashboard/invoices')
 }
