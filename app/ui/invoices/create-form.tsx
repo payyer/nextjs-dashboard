43f35b1d761cd6b41e58a4aebc3ec6ac4519d63a
payyer <quocanhle112@gmail.com>
2025-06-29 01:30:21 +0700

    athentication

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -14,7 +14,6 @@ import { createInvoice, State } from "@/app/lib/actions";
 export default function Form({ customers }: { customers: CustomerField[] }) {
   const initialState: State = { message: null, erorrs: {} };
   const [state, formAction] = useActionState(createInvoice, initialState);
-  console.log({ state });
   return (
     <form action={formAction}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
